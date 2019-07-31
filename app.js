3a733c6c90638635720dffc64f7b6b303ff9ae53
Kirill <kirill.cherkalov@gmail.com>
2019-07-31 22:57:19 +0300

    finished work with Handlebars

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs',
+  expressHbs({
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+    layoutsDir: 'views/layouts',
+    }),
+  );
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes =  require('./routes/shop');
 
-const rootDir = require('./util/path');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(rootDir, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
